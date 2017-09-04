1a79a30d9dd1d00e8bfa323436e9a66a8911f453
valdecanas <andres_ballesteros1993@hotmail.com>
2017-09-04 16:27:23 +0200

    cambio de proyecto

---
+++ src/CreadorTablas.java
@@ -0,0 +1,32 @@
+
+
+import java.util.ArrayList;
+
+public class CreadorTablas {
+	public static void main(String[] args) {
+
+		Field fieldA = new Field(1, "Canarias");
+		Field fieldB = new Field(5, "Murcia");
+
+		Usuarios usuario = new Usuarios("TablaUsers");
+		usuario.addField(fieldA);
+		usuario.addField(fieldB);
+
+		// Comprobacion-Ejemplo:
+		ArrayList<Field> misFields = new ArrayList<Field>();
+		misFields = usuario.getFields();
+		Field field = null;
+		int tipo;
+		String campo;
+		for (int i = 0; i < misFields.size(); i++) {
+			field = misFields.get(i);
+			tipo = field.getTipo();
+			campo = field.getCampo();
+			
+			System.out.println("El field es: " + field);
+			System.out.println("El tipo es: " + tipo);
+			System.out.println("El campo es: " + campo);
+			
+		}
+	}
+}
---
+++ src/Usuarios.java
@@ -0,0 +1,18 @@
+
+
+public class Usuarios extends Table {
+
+	protected String nombre;
+	
+	
+	public Usuarios(String nombre) {
+		super();
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String getTable() {
+		return this.nombre;
+	}
+
+}
